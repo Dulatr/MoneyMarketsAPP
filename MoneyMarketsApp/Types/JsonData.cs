9ff75b99da3fa9b18b85f1ac84de2df007bb5daf
Dulatr <echo.dulatr@gmail.com>
2020-06-08 19:55:42 -0400

    Add a JsonData class
    * Start of overview for return types

---
+++ MoneyMarketsApp/Types/JsonData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace MoneyMarketsApp.Types
+{
+    public class JsonData: Dictionary<string,string[]>
+    {
+        public JsonData() : base() { }
+
+        public string[] DOW { get; set; }
+        public string[] NASDAQ { get; set; }
+        public string[] SP { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
