1cbd55526ce568981321a7a01c6cc10819b1124b
Dulatr <echo.dulatr@gmail.com>
2020-06-08 20:27:10 -0400

    Overview page read from JSON
    * Have the Points bindings updated
      by the Overview - viewmodel.

---
+++ MoneyMarketsApp/ViewModel/MarketOverviewVM.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using GalaSoft.MvvmLight;
+using Newtonsoft.Json;
+
+namespace MoneyMarketsApp.ViewModel
+{
+    public class MarketOverviewVM : ViewModelBase
+    {
+        public MarketOverviewVM()
+        {
+            readJson();
+        }   
+        
+        private async void readJson()
+        {
+            using (StreamReader sr = new StreamReader(@"F://ExternalCoderProjects/Python/MM_App/MoneyMarketsCLI/data/market_overview.json"))
+            {
+                string content = await sr.ReadToEndAsync();
+
+                Dictionary<string, string[]> data = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(content);
+
+                DOWPoints = data["Dow"][0];
+                NASDAQPoints = data["Nasdaq"][0];
+                SPPoints = data["S&P"][0];
+            }
+        }
+
+        #region Points Data
+        private string dowPoints;
+        public string DOWPoints
+        {
+            get => dowPoints;
+            set
+            {
+                dowPoints = value;
+                RaisePropertyChanged("DOWPoints");
+            }
+        }
+        private string nasPoints;
+        public string NASDAQPoints
+        {
+            get => nasPoints;
+            set
+            {
+                nasPoints = value;
+                RaisePropertyChanged("NASDAQPoints");
+            }
+        }
+        private string spPoints;
+        public string SPPoints
+        {
+            get => spPoints;
+            set
+            {
+                spPoints = value;
+                RaisePropertyChanged("SPPoints");
+            }
+        }
+        #endregion
+
+    }
+}
