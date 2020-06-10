5fbd4ec1dc3bdd84032bbc1fb6211a1daa134064
Dulatr <echo.dulatr@gmail.com>
2020-06-09 22:07:49 -0400

    Include Stock model and VM
    *  TableView - viewmodel addition
    
    * Stock data type
    
    * Update project file to include references

---
+++ MoneyMarketsApp/Types/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMarketsApp.Types
+{
+    public class Stock
+    {
+        public string Ticker { get; set; }
+        public string Price { get; set; }
+        
+        public Stock()
+        {
+            Ticker = "";
+            Price = "";
+        }
+    }
+}
---
+++ MoneyMarketsApp/ViewModel/TableViewVM.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GalaSoft.MvvmLight;
+using MoneyMarketsApp.Types;
+
+namespace MoneyMarketsApp.ViewModel
+{
+    public class TableViewVM : ViewModelBase
+    {
+        public TableViewVM()
+        {
+            currentSelection = TableOptions[0];
+            data = new List<Stock>();
+            data.Add(new Stock() { Price = "12", Ticker = "AAPL" });
+            data.Add(new Stock() { Price = "15", Ticker = "MMM" });
+        }
+
+        public List<string> TableOptions
+        {
+            get
+            {
+                return new List<string>() { "DOW", "NASDAQ", "S&P 500" };
+            }
+        }
+        private string currentSelection;
+        public string CurrentTableSelection
+        {
+            get => currentSelection;
+            set
+            {
+                currentSelection = value;
+                RaisePropertyChanged("CurrentTableSelection");
+                RaisePropertyChanged("Data");
+            }
+        }
+
+        private List<Stock> data; 
+        public List<Stock> Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                RaisePropertyChanged("data");
+            }
+        }
+    }
+}
