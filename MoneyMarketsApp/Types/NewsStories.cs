558762c3b347705011981373b7916b4cfb70f201
Dulatr <echo.dulatr@gmail.com>
2020-06-17 02:01:18 -0400

    Refactor for v0.2.2 CLI patch
    * Adds two new objects:
        - Story, StoryCollection
    
    * Bitmap image path is required, so
      this patch is critical for story list
      populating.

--- MoneyMarketsApp/Types/NewsStories.cs
+++ MoneyMarketsApp/Types/NewsStories.cs
@@ -8,10 +8,16 @@ using System.IO;
 namespace MoneyMarketsApp.Types
 {
 
-    public class NewsStories
+    public class Story
     {
         public string title { get; set; }
         public string path{ get; set; }
         public string web_link { get; set; }
+        public string img_link { get; set; }
+    }
+
+    public class StoryCollection : JsonData
+    {
+        public Dictionary<string, Dictionary<string, Dictionary<string,string>>> stories { get; set; }
     }
 }
--- MoneyMarketsApp/ViewModel/HomePageVM.cs
+++ MoneyMarketsApp/ViewModel/HomePageVM.cs
@@ -1,5 +1,7 @@
 ﻿using GalaSoft.MvvmLight;
+using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,13 +14,50 @@ namespace MoneyMarketsApp.ViewModel
     {
         public HomePageVM()
         {
-            homeStories = new List<NewsStories>();
-            homeStories.Add(new NewsStories() { title = "Story 1", path= "C:/Users/Tyler Dula/Documents/Temporary/img/2.PNG"});
-            homeStories.Add(new NewsStories() { title="Story 2", path= "C:/Users/Tyler Dula/Documents/Temporary/img/bro.PNG" });
+            homeStories = new List<Story>();
+            collect_data_background();
+            //homeStories.Add(new NewsStories() { title = "Story 1", path= "C:/Users/Tyler Dula/Documents/Temporary/img/2.PNG"});
+            //homeStories.Add(new NewsStories() { title="Story 2", path= "C:/Users/Tyler Dula/Documents/Temporary/img/bro.PNG" });
         }
+        private async void collect_data_background()
+        {
+            string stdout;
 
-        public List<NewsStories> homeStories;
-        public List<NewsStories> HomeStories
+            using (Process money = new Process())
+            {
+                money.StartInfo.UseShellExecute = false;
+                money.StartInfo.CreateNoWindow = true;
+                money.StartInfo.RedirectStandardError = true;
+                money.StartInfo.RedirectStandardOutput = true;
+                string path_variable = Environment.GetEnvironmentVariable("MONEY_MARKETS");
+                Console.WriteLine(path_variable);
+                money.StartInfo.FileName = path_variable + "/money.exe";
+                money.StartInfo.Arguments = "-i story --front-page";
+                money.Start();
+                stdout = await money.StandardOutput.ReadToEndAsync();
+
+                ParseData(stdout);
+
+            }
+        }
+        private void ParseData(string response)
+        {
+            var stories = JsonConvert.DeserializeObject<StoryCollection>(response);
+            foreach (string page in stories.stories.Keys)
+            {
+                foreach(string story in stories.stories[page].Keys)
+                {
+                    homeStories.Add(new Story() {
+                        title = stories.stories[page][story]["title"],
+                        path = stories.stories[page][story]["path"],
+                    });
+                }
+
+            }
+            RaisePropertyChanged("HomeStories");
+        }
+        public List<Story> homeStories;
+        public List<Story> HomeStories
         {
             get => homeStories;
         }
